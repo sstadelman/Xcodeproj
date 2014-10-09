686b84dbd5a0763bd86f1a14411442a35b3794c0
Boris Bügling <boris@icculus.org>
2014-10-09 11:54:01 +0200

    Xcode ¯\_(ツ)_/¯

--- lib/xcodeproj/plist_helper.rb
+++ lib/xcodeproj/plist_helper.rb
@@ -570,7 +570,7 @@ module DevToolsCore
         CoreFoundation.NSSelectorFromString(CoreFoundation.RubyStringToCFString('writeToFileSystemProjectFile:userFile:checkNeedsRevert:')),
         1,
         0,
-        0)
+        1)
     end
 
     private
